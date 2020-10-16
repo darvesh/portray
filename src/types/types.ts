5513417c4a00f9a78aaf35ed8ec2bbcbe455c192
Darvesh <drvsh@protonmail.com>
2020-10-17 05:28:27 +0530

    fix: themes type

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,40 +5,10 @@ export interface Options {
 	fontPath?: string;
 	windowControl?: 0 | 1 | 2;
 	type?: "stream" | "buffer";
-	theme?: ThemesTyp;
+	theme?: ThemesType;
 	borderColor?: string;
 	borderSize?: number;
 	language?: string;
 }
 
-export type ThemesTyp = keyof typeof Themes;
-export type ThemesType =
-	| "3024-night"
-	| "a11y-dark"
-	| "blackboard"
-	| "base16-dark"
-	| "base16-light"
-	| "cobalt"
-	| "dracula"
-	| "duotone-dark"
-	| "hopscotch"
-	| "lucario"
-	| "material"
-	| "monokai"
-	| "night-owl"
-	| "nord"
-	| "oceanic-next"
-	| "one-light"
-	| "one-dark"
-	| "panda-syntax"
-	| "paraiso-dark"
-	| "seti"
-	| "shades-of-purple"
-	| "solarized dark"
-	| "solarized light"
-	| "synthwave-84"
-	| "twilight"
-	| "verminal"
-	| "vscode"
-	| "yeti"
-	| "zenburn";
+export type ThemesType = keyof typeof Themes;
