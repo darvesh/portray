fb41d38286a1914bf6382e79c645e1065d0480b3
Darvesh <drvsh@protonmail.com>
2020-10-24 17:26:36 +0530

    temp-fix: wrap

--- src/template/template.ts
+++ src/template/template.ts
@@ -6,7 +6,11 @@ type Options = {
 	fontSize: number;
 	windowControl: string;
 };
-export const generateHTML = (code: string, css: string, options: Options): string => `\
+export const generateHTML = (
+	code: string,
+	css: string,
+	options: Options
+): string => `\
 <!DOCTYPE html>
 <html>
     <head>
@@ -19,40 +23,62 @@ export const generateHTML = (code: string, css: string, options: Options): strin
                 font-family: FiraCode;
                 src: url("${options.fontPath}") format("truetype"); 
             }
-            body {
-                border: ${options.borderSize}px solid ${options.borderColor};
-                margin: 0;
-                padding: 1em;
-                text-rendering: optimizeLegibility
-                position: absolute;
+            * {
+              margin: 0;
+              padding: 0;  
             }
-             .container{
-                margin: auto;
-                padding:0;
+            body {
+                background: #bbb;
+                margin: 0;
+                padding: 0;
+                display: inline-block;
+            }
+            .container {
+                margin: ${options.borderSize}px;
                 border-radius: 5px;
                 box-shadow: rgba(0, 0, 0, 0.55) 0px 20px 68px;
-                padding: 1em;
+                padding: 1rem 1.5rem;
+                background: ${options.borderColor};
             }
-            
+            pre {
+                font-size: 20px;
+                font-family: CustomFont, FiraCode, monospace;
+                line-height: 1.5rem;
+                white-space: pre;
+            }
+
+            /*body {
+                border:  solid 
+                padding: 0;
+                margin: 0;
+                text-rendering: optimizeLegibility;
+            }
+             .container{
+                box-shadow: rgba(0, 0, 0, 0.55) 0px 20px 68px;
+                padding: 1em;
+                border-radius: 10px;
+                display:flex;
+                flex-direction: column;
+                flex-basis: content;
+            }
             pre {
                 font-size: ${options.fontSize}px;
                 font-family: CustomFont, FiraCode;
                 margin-left: 30px;
                 line-height: 1.5em;
                 white-space: pre;
-                width: 100%;
-            }
+            }*/
             ${css}
         </style>
     </head>
-    <body style="background:#ABB8C3;">
-        <div class="container hljs-background">
-            <div style="padding:10px;">
-                ${options.windowControl}
-            </div>
-            <code>
-                <pre class="hljs-text">${code}</pre>
-            </code>
-        </div>
+   <body>
+	    <div class="container hljs-background">
+                <div>
+                    ${options.windowControl}
+                </div>
+                <code>
+                    <pre class="hljs-text">${code}</pre>
+                </code>
+	    </div>
     </body>
 </html>`;
